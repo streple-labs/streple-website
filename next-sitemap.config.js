e8a9b7ff051a342d3d07791c021803201e6b20c6
Jude <silentcodear@gmail.com>
2025-09-07 14:36:02 +0100

    added the configuration that allows auto generation of sitemap and robots.txt postbuild

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,17 @@
+// next-sitemap.config.js
 module.exports = {
   siteUrl: 'https://streple.com',
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: 'weekly',
   priority: 0.7,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/*?s='],  // ⬅️ block search query pages
+      },
+    ],
+  },
 };
